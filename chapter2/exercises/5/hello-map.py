0d1b1d82a892e7cb6feed9b8eda0fea832bbd8db
danial.ramezanzadeh <danial.ramezanzadeh@L-P0-NET4058.part.local>
2025-09-20 14:44:11 +0330

    show counter for syscall types

--- chapter2/exercises/5/hello-map.py
+++ chapter2/exercises/5/hello-map.py
@@ -5,32 +5,31 @@ from time import sleep
 program = r"""
 BPF_HASH(counter_table);
 
-int hello(void *ctx) {
-   u64 uid;
+int hello(struct bpf_raw_tracepoint_args *ctx) {
+   u64 id = ctx->args[0];
    u64 counter = 0;
    u64 *p;
 
-   uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-   p = counter_table.lookup(&uid);
+   p = counter_table.lookup(&id);
    if (p != 0) {
       counter = *p;
    }
    counter++;
-   counter_table.update(&uid, &counter);
+   counter_table.update(&id, &counter);
    return 0;
 }
 """
 
 b = BPF(text=program)
-syscall = b.get_syscall_fnname("execve")
-b.attach_kprobe(event=syscall, fn_name="hello")
+# syscall = b.get_syscall_fnname("execve")
+# b.attach_kprobe(event=syscall, fn_name="hello")
 
 # Attach to a tracepoint that gets hit for all syscalls 
-# b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
+b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
 
 while True:
     sleep(2)
-    s = ""
-    for k,v in b["counter_table"].items():
-        s += f"ID {k.value}: {v.value}\t"
-    print(s)
+    print("=== Syscall Counters ===")
+    for k, v in b["counter_table"].items():
+        print(f"Syscall {k.value}: {v.value}")
+    print()
